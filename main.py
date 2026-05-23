8ddfd241d9b72230cdf249e83a491906348bc43d
ivan3321 <127299972+ivan3321@users.noreply.github.com>
2026-05-23 19:50:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS").split(",")]
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-
 @dp.chat_join_request()
 async def join_request(event: ChatJoinRequest):
     user = event.from_user
@@ -22,12 +21,10 @@ async def join_request(event: ChatJoinRequest):
     )
 
     for owner_id in OWNER_IDS:
-    await bot.send_message(owner_id, text)
-
+        await bot.send_message(owner_id, text)
 
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())

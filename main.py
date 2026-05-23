c826e524f70dce48b35f61990b023752d2f17ddc
ivan3321 <127299972+ivan3321@users.noreply.github.com>
2026-05-23 18:56:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import ChatJoinRequest
+import asyncio
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.chat_join_request()
+async def join_request(event: ChatJoinRequest):
+    user = event.from_user
+
+    text = (
+        f"Новая заявка\n\n"
+        f"Имя: {user.full_name}\n"
+        f"ID: {user.id}\n"
+        f"Username: @{user.username}"
+    )
+
+    await bot.send_message(OWNER_ID, text)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
